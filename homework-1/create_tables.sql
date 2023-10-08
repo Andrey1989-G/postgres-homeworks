db8e45c6cad7370264d3fafbbdcb3de427d4128f
Andrey1989-G <goreglyad.andrey@gmail.com>
2023-10-08 15:32:28 +0500

    HW1 и HW2 готово

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,32 @@
 -- SQL-команды для создания таблиц
+
+-- DROP TABLE orders
+-- DROP TABLE customers
+-- DROP TABLE employees
+
+-- клиенты
+CREATE TABLE customers (
+  customer_id VARCHAR(50) PRIMARY KEY,
+  company_name VARCHAR(100) NOT NULL,
+  contact_name VARCHAR(50) NOT NULL
+);
+
+--сотрудники
+CREATE TABLE employees (
+  employee_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  title VARCHAR(100),
+  birth_date VARCHAR(50),
+  notes TEXT
+);
+
+--заказы. связь и с сотрудниками и с клиентами
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    --название/ тип / REFERENCES / название таблицы / (название строки) / NOT NULL,
+    customer_id VARCHAR(50) NOT NULL,
+    employee_id VARCHAR(50) NOT NULL,
+    order_date VARCHAR(50) NOT NULL,
+    ship_city VARCHAR(50) NOT NULL
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,55 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import csv
+import psycopg2
+from password import password
+
+# Подключение к db
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password=password
+)
+try:
+    with conn:
+        with conn.cursor() as cur:
+
+            # Заполнение таблицы customers
+            with open('north_data/customers_data.csv', 'r') as f:
+                reader = csv.reader(f)
+                # next(reader)  # Пропуск заголовка
+                for row in reader:
+                    cur.execute(
+                        "INSERT INTO customers (customer_id, company_name, contact_name) "
+                        "VALUES (%s, %s, %s)",
+                        (row[0], row[1], row[2])
+                    )
+            #
+            # Заполнение таблицы employees
+            with open('north_data/employees_data.csv', 'r') as f:
+                reader = csv.reader(f)
+                next(reader)  # Пропуск заголовка
+                for row in reader:
+                    cur.execute(
+                        "INSERT INTO employees (first_name, last_name, title, birth_date, notes) "
+                        "VALUES (%s, %s, %s, %s, %s)",
+                        (row[1], row[2], row[3], row[4], row[5])
+                    )
+
+
+            # Заполнение таблицы orders
+
+            # открываем файл и читаем данные
+            with open('north_data/orders_data.csv', 'r') as f:
+                reader = csv.reader(f)
+                next(reader)  # Пропуск заголовка
+                for row in reader:
+                    cur.execute(
+                        "INSERT INTO orders (order_id, customer_id,employee_id,order_date,ship_city) "
+                        "VALUES (%s, %s, %s, %s, %s)",
+                        (row[0], row[1], row[2], row[3], row[4]) # исправляем индексы столбцов и пропускаем order_id
+                    )
+finally:
+    #закрытие соединения
+    conn.close()
